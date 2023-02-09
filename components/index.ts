5ec5dabc53ad4854d2c98c75e93b07f09d0cc6f1
Marie Bawanan <bawananmarieflor@gmail.com>
2023-02-09 18:35:18 +0800

    chore: clean up unused files

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
-export { default as About } from "./About";
-export { default as Contact } from "./Contact";
-export { default as Footer } from "./Footer";
+export * from "./Badge";
+export * from "./Clipboard";
 export { default as Hero } from "./Hero";
 export * from "./Layout";
 export * from "./LogRocket";
